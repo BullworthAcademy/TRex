58ffe965ee347502b592e7f1f158c7cd5ecec667
Maike <sedir@Sedirs-iMac-Pro.local>
2021-07-07 16:37:39 +0200

    evaluation details

--- TRex-Java-client/generate_events.py
+++ TRex-Java-client/generate_events.py
@@ -1,30 +1,25 @@
+#!/usr/bin/env python3
 import random
 import os
 import time
 
-load = 0.1  # 10% Smoke_x events
-attempts = 10
-input_rate_per_second = 100
 
-for load in [0.1, 0.5, 0.9]:
+def publish_events(seconds, load, attempts):
     for i in range(attempts):
-        os.system(f"java -jar TRex-client.jar localhost 50254 -pub 2626 attempt {i} scenario {load}")
-        start_time = time.time()
         n_call = 0
-        while time.time() - start_time < 15 or n_call == 0 < input_rate_per_second:
+        t_end = time.time() + seconds
+        while time.time() < t_end:
             n_call += 1
             temperature = round(random.uniform(0,100))
             composite_event_id = random.randint(1, 10)
             smoke_id = composite_event_id * 1000
             temp_id = composite_event_id * 1000 + 1
             if random.random() > load:
-              os.system(f"java -jar TRex-client.jar localhost 50254 -pub {temp_id} area toto value {temperature}")
+                os.system(f"java -jar TRex-client.jar localhost 50254 -pub {temp_id} area toto value {temperature}")
             else:
-              os.system(f"java -jar TRex-client.jar localhost 50254 -pub {smoke_id} area toto")
-            if n_call >= input_rate_per_second:
-                print("über input_rate_per_second")
-            if time.time() - start_time > 1:
-                print(f"über Sekunden {n_call}")
-        time.sleep(20)
-    
-    
+                os.system(f"java -jar TRex-client.jar localhost 50254 -pub {smoke_id} area toto")
+        print(f"Attempt {i+1}: {n_call/seconds} events/seconds")
+
+
+if __name__ == "__main__":
+    publish_events(seconds=10, load=0.9, attempts=3)
--- TRex-Java-client/generate_rules.py
+++ TRex-Java-client/generate_rules.py
@@ -31,8 +31,8 @@ for i in range(10):
     for temp_threshold in range(1,101):
         with open(f"trex_rules/trex2_{file_idx}.rule", mode="w", encoding="utf-8") as rule_file:
             rule_file.write(assign_line)
-            rule1 = f"""Define  Fire{idx}(area: string, measuredTemp: float)
-From    Smoke{idx}(area=>$a) and each Temp{idx}([string]area=$a, value>{temp_threshold}) within 300000 from Smoke{idx}
+            rule2 = f"""Define  Fire{idx}(area: string, measuredTemp: float)
+From    Smoke{idx}(area=>$a) and last Temp{idx}([string]area=$a, value>{temp_threshold}) within 300000 from Smoke{idx}
 Where   area:=Smoke{idx}.area, measuredTemp:=Temp{idx}.value;"""
             rule_file.write("\n\n")
             rule_file.write(rule1)
