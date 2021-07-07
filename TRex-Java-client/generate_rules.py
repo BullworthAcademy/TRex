2bf60c67d9aebb87e55cff181bf71f0e93b3ecf2
Maike <sedir@Sedirs-iMac-Pro.local>
2021-07-07 11:55:36 +0200

    added scripts

---
+++ TRex-Java-client/generate_rules.py
@@ -0,0 +1,39 @@
+smoke_id = 1000
+temp_id = 1001
+fire_id = 1100
+file_idx = 0
+for i in range(10):
+    idx = i+1
+    assign_line = f"Assign {smoke_id} => Smoke{idx}, {temp_id} => Temp{idx}, {fire_id} => Fire{idx}"
+    smoke_id += 1000
+    temp_id += 1000
+    fire_id += 1000
+    for temp_threshold in range(1,101):
+        with open(f"trex_rules/trex1_{file_idx}.rule", mode="w", encoding="utf-8") as rule_file:
+            rule_file.write(assign_line)
+            rule1 = f"""Define  Fire{idx}(area: string, measuredTemp: float)
+From    Smoke{idx}(area=>$a) and each Temp{idx}([string]area=$a, value>{temp_threshold}) within 300000 from Smoke{idx}
+Where   area:=Smoke{idx}.area, measuredTemp:=Temp{idx}.value;"""
+            rule_file.write("\n\n")
+            rule_file.write(rule1)
+            file_idx += 1
+
+smoke_id = 1000
+temp_id = 1001
+fire_id = 1100
+file_idx = 0
+for i in range(10):
+    idx = i+1
+    assign_line = f"Assign {smoke_id} => Smoke{idx}, {temp_id} => Temp{idx}, {fire_id} => Fire{idx}"
+    smoke_id += 1000
+    temp_id += 1000
+    fire_id += 1000
+    for temp_threshold in range(1,101):
+        with open(f"trex_rules/trex2_{file_idx}.rule", mode="w", encoding="utf-8") as rule_file:
+            rule_file.write(assign_line)
+            rule1 = f"""Define  Fire{idx}(area: string, measuredTemp: float)
+From    Smoke{idx}(area=>$a) and each Temp{idx}([string]area=$a, value>{temp_threshold}) within 300000 from Smoke{idx}
+Where   area:=Smoke{idx}.area, measuredTemp:=Temp{idx}.value;"""
+            rule_file.write("\n\n")
+            rule_file.write(rule1)
+            file_idx += 1
---
+++ TRex-Java-client/load_rule2.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Bash script for loading the rule files corresponding to rule 1
+ 
+for ((i = 0; i < 1000; i++)); do
+        java -jar TRex-client.jar localhost 50254 -rule "trex_rules/trex2_$i.rule"
+done
